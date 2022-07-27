e40382344b2c105e9ce5726b7decc4bff4e93fea
Aditri-Sures <96643325+Aditri-Sures@users.noreply.github.com>
2022-07-27 18:36:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+clownnose=loadImage("https://i.postimg.cc/rwK31dXj/moustache-PNG18.png");
+}
+
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+    }
+}
+function draw(){
+image(video,0,0,300,300);
+image(clownnose,noseX-160,noseY-135,30,30);
+}
+
+function take_snapshot(){
+    save('crown.png');
+}
